cd11b1ad99020f7c96e6b87694e096a5b0a779e5
FerbaDev <elferbarron1@gmail.com>
2024-05-17 01:30:01 -0300

    se corrigen rutas de cart

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -7,13 +7,13 @@ const cartController = new CartController();
 
 router.use(authMiddleware);
 
-router.post("/", cartController.nuevoCarrito);
-router.get("/:cid", cartController.obtenerProductosDeCarrito);
-router.post("/:cid/product/:pid", cartController.agregarProductoEnCarrito);
-router.delete('/:cid/product/:pid', cartController.eliminarProductoDeCarrito);
-router.put('/:cid', cartController.actualizarProductosEnCarrito);
-router.put('/:cid/product/:pid', cartController.actualizarCantidad);
-router.delete('/:cid', cartController.vaciarCarrito);
+router.post("/", cartController.createCart);
+router.get("/:cid", cartController.getProductsFromCart);
+router.post("/:cid/product/:pid", cartController.addProductToCart);
+router.delete('/:cid/product/:pid', cartController.deleteProductFromCart);
+router.put('/:cid', cartController.updateProductsInCart);
+router.put('/:cid/product/:pid', cartController.updateQuantity);
+router.delete('/:cid', cartController.emptyCart);
 router.post('/:cid/purchase', cartController.finalizarCompra);
 
 
