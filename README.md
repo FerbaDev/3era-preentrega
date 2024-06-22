85a02019fdf1f11bf833c5dde5453ee2994f6319
FerbaDev <elferbarron1@gmail.com>
2024-06-22 17:48:12 -0300

    agregando envio de mail

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 # 3era Preentrega Backend
 
 
+.env.desarrollo
+MONGO_URL=mongodb+srv://talba:talba@clustertalba.vnmlpsv.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=ClusterTalba
 
-
-
-
-
-
-
-
-explicación de la 3era preentrega -----clase 16 01:37
+.env.produccion
+MONGO_URL=mongodb://localhost:27017/ecommerce
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -57,10 +57,6 @@ class UserController {
             req.session.login = true;
             req.session.user = {...usuarioEncontrado._doc};
                 
-            // res.cookie("coderCookieToken", {
-            //     maxAge: 3600000,
-            //     httpOnly: true
-            // });
 
             res.redirect("/api/users/profile");
         } catch (error) {
@@ -69,11 +65,22 @@ class UserController {
         }
     }
 
+    // async profile(req, res) {
+    //     //Con DTO: 
+    //     const userDto = new UserDTO(req.user.first_name, req.user.last_name, req.user.role);
+    //     const isAdmin = req.user.role === 'admin';
+    //     res.render("profile", { user: userDto, isAdmin });
+    // }
     async profile(req, res) {
-        //Con DTO: 
-        const userDto = new UserDTO(req.user.first_name, req.user.last_name, req.user.role);
-        const isAdmin = req.user.role === 'admin';
-        res.render("profile", { user: userDto, isAdmin });
+        try {
+            const isPremium = req.user.role === 'premium';
+            const userDto = new UserDTO(req.user.first_name, req.user.last_name, req.user.role);
+            const isAdmin = req.user.role === 'admin';
+
+            res.render("profile", { user: userDto, isPremium, isAdmin });
+        } catch (error) {
+            res.status(500).send('Error interno del servidor');
+        }
     }
 
     async logout(req, res) {
@@ -87,6 +94,77 @@ class UserController {
             return res.status(403).send("Acceso denegado");
         }
         res.render("admin");
+    }
+
+    async requestPasswordReset(req, res) {
+        const { email } = req.body;
+
+        try {
+            // Buscar al usuario por su correo electrónico
+            const user = await UserModel.findOne({ email });
+            if (!user) {
+                return res.status(404).send("Usuario no encontrado");
+            }
+
+            // Generar un token 
+            const token = generateResetToken();
+
+            // Guardar el token en el usuario
+            user.resetToken = {
+                token: token,
+                expiresAt: new Date(Date.now() + 3600000) // 1 hora de duración
+            };
+            await user.save();
+
+            // Enviar correo electrónico con el enlace de restablecimiento utilizando EmailService
+            await emailManager.enviarCorreoRestablecimiento(email, user.first_name, token);
+
+            res.redirect("/confirmacion-envio");
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Error interno del servidor");
+        }
+    }
+
+    async resetPassword(req, res) {
+        const { email, password, token } = req.body;
+
+        try {
+            // Buscar al usuario por su correo electrónico
+            const user = await UserModel.findOne({ email });
+            if (!user) {
+                return res.render("passwordcambio", { error: "Usuario no encontrado" });
+            }
+
+            // Obtener el token de restablecimiento de la contraseña del usuario
+            const resetToken = user.resetToken;
+            if (!resetToken || resetToken.token !== token) {
+                return res.render("passwordreset", { error: "El token de restablecimiento de contraseña es inválido" });
+            }
+
+            // Verificar si el token ha expirado
+            const now = new Date();
+            if (now > resetToken.expiresAt) {
+                // Redirigir a la página de generación de nuevo correo de restablecimiento
+                return res.redirect("/passwordcambio");
+            }
+
+            // Verificar si la nueva contraseña es igual a la anterior
+            if (isValidPassword(password, user)) {
+                return res.render("passwordcambio", { error: "La nueva contraseña no puede ser igual a la anterior" });
+            }
+
+            // Actualizar la contraseña del usuario
+            user.password = createHash(password);
+            user.resetToken = undefined; // Marcar el token como utilizado
+            await user.save();
+
+            // Renderizar la vista de confirmación de cambio de contraseña
+            return res.redirect("/login");
+        } catch (error) {
+            console.error(error);
+            return res.status(500).render("passwordreset", { error: "Error interno del servidor" });
+        }
     }
 }
 
